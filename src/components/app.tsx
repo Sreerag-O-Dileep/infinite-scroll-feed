44269a730e98eba919ff51ce3fc909ae96c9de0c
Sreerag-O-Dileep <35340906+Sreerag-O-Dileep@users.noreply.github.com>
2023-01-20 00:17:41 -0800

    initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,12 @@
+import "./app.css";
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="component-app">
+      <p>The app is working</p>
+    </div>
+  );
+};
+
+export { App };
